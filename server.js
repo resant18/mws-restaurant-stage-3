021cf38857a051995fe81d10bbec2290ee382cba
resant18 <santoso.renata@gmail.com>
2020-03-23 20:24:31 -0700

    add logging port

--- server.js
+++ server.js
@@ -5,4 +5,5 @@ var server = express();
 server.use("/", express.static(__dirname + "/"));
 
 const port = process.env.PORT || 8000;
+console.log(`port is ${port}`);
 server.listen(port, () => console.log(`Server is running on port ${port}`));
